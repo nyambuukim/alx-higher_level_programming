aa7125c63ec5baabe09d777ad13894f744ef7b7d
nyambuukim <nyambuukim1@gmail.com>
2023-11-20 07:04:36 -0800

    6-raise_exception_msg.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list(mylist=[], x=0):
+def safe_print_list(my_list=[], x=0):
     counter = 0
-    for index in range(x):
-	try:
-	    print("{}".format(my_list=[index], end="")
-	except IndexError:
-	    break
-	else:
-	    counter += 1
-	print()
-	return counter
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        else:
+            counter += 1
+    print()
+    return counter
